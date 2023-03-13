54bb53ac30ffb6486eda4478008393399402e4a9
bananawrap <ottoroos@protonmail.com>
2023-03-13 14:51:54 +0200

    tilaus ikkunan pohja

---
+++ js/ordering.js
@@ -0,0 +1,8 @@
+
+function open_ordering_overlay() {
+    document.getElementById("overlay").style.display = "block"
+}
+
+function close_ordering_overlay() {
+    document.getElementById("overlay").style.display = "none"
+}
